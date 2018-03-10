e7797c209ecd66c87dbd5607b1651e6853af7f8d
Ogamiq <miloszogaza@op.pl>
2018-03-10 13:21:50 +0100

    changed ctrl_users and package.json

--- api/controllers/ctrl_users.js
+++ api/controllers/ctrl_users.js
@@ -4,36 +4,6 @@ const bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 var User = mongoose.model('User');
 
-/*module.exports.authenticate = function(req, res, next){
-console.log('token');
-  var headerExists=req.headers.authorization;
-  if (headerExists) {
-    var token = req.headers.authorization.split(' ')[1]; //Authorization Bearer xxx
-    jwt.verify(token, 's3cr3t', function(error, decoded){
-      if (error){
-        console.log(error);
-        res.status(401).json('Unauthorized');
-      }else {
-        console.log('nu i cho tam?');
-        req.user = decoded.username;
-        next();
-      }
-    });
-  }else{
-    res.status(403).json('No token provided');
-  }
-};*/
-
-// module.exports.verify_user = function(token, hash){
-//   var decoded = jwt.verify(token, hash);
-//     if(User.findById(decoded.id)){
-//       return true;
-//     }
-//     else {
-//       return false;
-//     }  };
-
-
 module.exports.verify_token = (req,res,next) => {
   // Get auth header value
   const bearerHeader = req.headers['authorization'];
